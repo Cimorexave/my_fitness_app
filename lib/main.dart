40ecc8aef90b95d94b4519c1004cb2e819216a4e
Cimorexave <sadeq.s502@gmail.com>
2023-11-28 16:14:06 +0330

    dialog polished

--- lib/main.dart
+++ lib/main.dart
@@ -9,44 +9,60 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.lightGreen[200],
-            title: const Text('my fitness app'),
-          ),
-          body: const Center(
-            child: Text("Center"),
-          ),
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.lightGreen[200],
-            focusColor: Colors.lightGreen[100],
-            onPressed: () => {
-              showDialog(
-                  context: context,
-                  builder: (context) => const AddRecordWidget())
-            },
-            child: const Icon(Icons.add),
-          ),
-        ),
+    return const MaterialApp(
+      title: 'my fittness app',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightGreen[200],
+        title: const Text('my fitness app'),
+      ),
+      body: const Center(
+        child: Text("Center"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightGreen[200],
+        focusColor: Colors.lightGreen[100],
+        onPressed: () => {
+          showDialog(
+            context: context,
+            builder: (context) => const AddRecordStatelessDialog(),
+          )
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
 }
 
-class AddRecordWidget extends StatefulWidget {
-  const AddRecordWidget({super.key});
+class AddRecordStatelessDialog extends StatelessWidget {
+  const AddRecordStatelessDialog({super.key});
 
-  @override
-  State<AddRecordWidget> createState() => _AddRecordWidgetState();
-}
-
-class _AddRecordWidgetState extends State<AddRecordWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Center Dialog'),
+    return AlertDialog(
+      backgroundColor: Colors.green[50],
+      title: const Text('Add New Record'),
+      content: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: MediaQuery.of(context).size.height * 0.5,
+        child: const Text('content center child'),
+      ),
+      actions: [
+        ElevatedButton(
+            onPressed: () => {Navigator.of(context).pop()},
+            child: const Text('Cancel')),
+        ElevatedButton(onPressed: () => {}, child: const Text('Add')),
+      ],
     );
   }
 }
