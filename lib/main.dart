01b22e13bb5065b8398b988bacd005bdbe13583a
Cimorexave <sadeq.s502@gmail.com>
2023-11-28 21:47:50 +0330

    setup provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => SharedState(),
+    child: const MainApp(),
+  ));
+}
+
+class SharedState with ChangeNotifier {
+  final Map<String, String?> _formState = {
+    'title': null,
+    'calories': null,
+    'description': null,
+  };
+
+  Map<String, String?> get formState => _formState;
+  void setForm(Map<String, String> newFormState) {
+    _formState["title"] = newFormState["title"];
+    _formState["calories"] = newFormState["calories"];
+    _formState["description"] = newFormState["description"];
+
+    notifyListeners();
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -47,6 +68,10 @@ class HomePage extends StatelessWidget {
 class AddRecordStatelessDialog extends StatelessWidget {
   const AddRecordStatelessDialog({super.key});
 
+  void submitForm() {
+    print('Submitting Form');
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
@@ -55,14 +80,71 @@ class AddRecordStatelessDialog extends StatelessWidget {
       content: SizedBox(
         width: MediaQuery.of(context).size.width * 0.8,
         height: MediaQuery.of(context).size.height * 0.5,
-        child: const Text('content center child'),
+        child: const AddRecordForm(),
       ),
       actions: [
         ElevatedButton(
             onPressed: () => {Navigator.of(context).pop()},
             child: const Text('Cancel')),
-        ElevatedButton(onPressed: () => {}, child: const Text('Add')),
+        ElevatedButton(
+            onPressed: () {
+              submitForm();
+            },
+            child: const Text('Add')),
       ],
+    );
+  }
+}
+
+class AddRecordForm extends StatefulWidget {
+  const AddRecordForm({super.key});
+
+  @override
+  State<AddRecordForm> createState() => _AddRecordFormState();
+}
+
+class _AddRecordFormState extends State<AddRecordForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'Title:'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Title cannot be empty.";
+              }
+              return null;
+            },
+            onSaved: (value) => print('title: $value'),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'Calories:'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Value can't be empty.";
+              }
+              if (int.tryParse(value) is int) {
+                return "Value must be a number.";
+              }
+              return null;
+            },
+            onSaved: (value) => print('calories: $value'),
+          ),
+          TextFormField(
+            decoration:
+                const InputDecoration(labelText: 'Description (optional):'),
+            validator: (value) {
+              return null;
+            },
+            onSaved: (value) => print('description: $value'),
+          ),
+        ],
+      ),
     );
   }
 }
