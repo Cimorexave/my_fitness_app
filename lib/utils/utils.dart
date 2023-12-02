10c669c28b195aff4811244d3fdcdaa24d421750
Cimorexave <sadeq.s502@gmail.com>
2023-12-02 21:54:01 +0330

    format date function

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,4 +1,5 @@
 import "../model/record.dart";
+import 'package:intl/intl.dart';
 
 Map<DateTime, List<Record>> groupRecordsByDay(List<Record> records) {
   var groupedRecords = <DateTime, List<Record>>{};
@@ -27,4 +28,20 @@ bool isSameDay(DateTime date1, DateTime date2) {
   return date1.year == date2.year &&
       date1.month == date2.month &&
       date1.day == date2.day;
+}
+
+String formatDate(DateTime date) {
+  DateTime now = DateTime.now();
+  DateTime today = DateTime(now.year, now.month, now.day);
+  DateTime yesterday = DateTime(now.year, now.month, now.day - 1);
+
+  String formattedDate = DateFormat('yyyy/MM/dd, EEEE').format(date);
+
+  if (date.isAtSameMomentAs(today)) {
+    return 'Today, $formattedDate';
+  } else if (date.isAtSameMomentAs(yesterday)) {
+    return 'Yesterday, $formattedDate';
+  } else {
+    return formattedDate;
+  }
 }
