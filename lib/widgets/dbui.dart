22381a777b845e9290b0267d6ed2ebf6b7b6c57d
Cimorexave <sadeq.s502@gmail.com>
2023-12-06 12:39:37 +0330

    dbui test widget

---
+++ lib/widgets/dbui.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
+class DBUI extends StatelessWidget {
+  final Box box;
+  const DBUI({super.key, required this.box});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
